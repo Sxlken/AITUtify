58b483aae81a542358bee72d8d61172f56c20524
Sxlken <149990761+Sxlken@users.noreply.github.com>
2024-03-15 14:48:16 +0600

    Add files via upload

---
+++ music_platform/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MusicDatabase database = new MusicDatabase();
+
+        UserInterface ui = new UserInterface(database);
+        ui.MainMenu();
+    }
+}
---
+++ music_platform/ConsoleApp1/music_database.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+public class MusicDatabase
+{
+    private List<Song> songs = new List<Song>();
+
+    public void AddSong(Song song)
+    {
+        songs.Add(song);
+    }
+
+    public List<Song> SearchByTitle(string title)
+    {
+        return songs.FindAll(song => song.Title.ToLower().Contains(title.ToLower()));
+    }
+
+}
+    
